12e180e5cde2e3b52d6e5653358e0b18815d1588
ANIL MN <amn.nmb@gmail.com>
2020-08-07 13:29:38 +0530

    Updates

---
+++ precache-manifest.46cb8c3837fc710789717a70d2efa956.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1eadb697a71c26b42c4cb60348b06a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f43f47696fd10276824e",
+    "url": "/static/css/2.1f35d23a.chunk.css"
+  },
+  {
+    "revision": "9ab011ad24641e14987a",
+    "url": "/static/css/main.a530b0a1.chunk.css"
+  },
+  {
+    "revision": "f43f47696fd10276824e",
+    "url": "/static/js/2.1d01e5a1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1d01e5a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab011ad24641e14987a",
+    "url": "/static/js/main.8990c351.chunk.js"
+  },
+  {
+    "revision": "a91f61a2b15a1b1cb956",
+    "url": "/static/js/runtime-main.380568c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
